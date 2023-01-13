4e93e9d1d3c734c7ecae97da6176e8393d12580a
Gamze Yazgeldi <101990822+gyazgeldi@users.noreply.github.com>
2023-01-13 14:21:09 +0200

    Update STRTN-UCSC-Allas.sh

--- STRTN-UCSC-Allas.sh
+++ STRTN-UCSC-Allas.sh
@@ -12,7 +12,10 @@ function usage() {
 Usage: ${PROGNAME} [-w <path (required)>]
 Options: 
 -w, --working	/PATH/to/the working directory. Required! 
--n, --name	The hub in Allas. (default: STRT2-hub)	
+-e, --email     Email address for hub file.
+-n, --name	Hub name to store in Allas or hosting service. (default: STRT2-hub)
+-h, --help      Show usage.
+-v, --version   Show version.
 EOS
 	exit 1
 }
@@ -25,7 +28,7 @@ EOS
 }
 
 # Default parameter.
-NAME_HUB=STRT2-hub
+NAME_HUB=STRTN-hub
 
 # Parameter settings
 PARAM=()
@@ -38,6 +41,15 @@ for opt in "$@"; do
 	    fi
 	    WorkingDir=true
 	    WorkingDir_PATH="$2"
+	    shift 2
+	    ;;
+	'-e' | '--email' )
+	    if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+		echo "${PROGNAME}: option requires an argument -- $( echo $1 | sed 's/^-*//' )" 1>&2
+		exit 1
+	    fi
+	    email=true
+	    EMAIL="$2"
 	    shift 2
 	    ;;
 	'-n' | '--name' )
@@ -74,6 +86,94 @@ fi
 [ "${WorkingDir}" != "true" ] && usage
 [ "${name_hub}" != "true" ] && usage
 
+# Creation the tracks for BAM files
+shortLabel_hub="STRT-N data sequencing library tracks"
+longLabel_hub="STRT-N data sequencing library tracks"
+useOneFile=on
+echo -e "hub "${NAME_HUB}"\n""shortLabel "${shortLabel_hub}"\n""longLabel "${longLabel_hub}"\n""useOneFile "${useOneFile}"\n""email "${EMAIL}"\n""\n""genome "${GENOME_VALUE}"\n" >> hub.txt
+
+readarray -t bam_array < <(printf '%s\n' *.output.bam | sort -V)
+
+# First barcode will be shown as example. Other barcodes will be hidden so one can set up configurations as desired.
+file=${bam_array[0]}
+name=$(basename $file .output.bam)
+longLabel=${name}", read alignments tracks"
+type=bam
+visibility=squish
+alwaysZero=on
+graphType=bar
+windowingFunction=mean
+bamColorMode=strand
+colorByStrand="0,0,255 255,0,0"
+bigDataUrl=$file
+echo -e "track "${name}"\n""shortLabel "${name}"\n""longLabel "$longLabel"\n""type "${type}"\n""bigDataUrl "${bigDataUrl}"\n""visibility "${visibility}"\n""alwaysZero "${alwaysZero}"\n""graphType "${graphType}"\n""windowingFunction "${windowingFunction}"\n""bamColorMode "${bamColorMode}"\n""colorByStrand "\"${colorByStrand}\""\n" >> hub.txt
+
+for file in "${bam_array[@]:1:47}"
+do
+    name=$(basename $file .output.bam)
+    longLabel=${name}", read alignments tracks"
+    type=bam
+    visibility=hide
+    alwaysZero=on
+    graphType=bar
+    windowingFunction=mean
+    bamColorMode=strand
+    colorByStrand="0,0,255 255,0,0"
+    bigDataUrl=${file}
+    echo -e "track "${name}"\n""shortLabel "${name}"\n""longLabel "${longLabel}"\n""type "${type}"\n""bigDataUrl "${bigDataUrl}"\n""visibility "${visibility}"\n""alwaysZero "${alwaysZero}"\n""graphType "${graphType}"\n""windowingFunction "${windowingFunction}"\n""bamColorMode "${bamColorMode}"\n""colorByStrand "\"${colorByStrand}\""\n" >> hub.txt
+done
+
+# Creation the tracks for bigWig files
+# BigWig files
+# First barcode will be shown as example.
+file=${bam_array[0]}
+name=$(basename $file .output.bam)
+longLabel=${name}", forward strand histogram tracks"
+type=bigWig
+visibility=full
+alwaysZero=on
+graphType=bar
+windowingFunction=mean
+color=0,0,100
+bigDataUrl=${name}_plus.bw
+echo -e "track "${name}_plus"\n""shortLabel "${name}"\n""longLabel "${longLabel}"\n""type "${type}"\n""bigDataUrl "${bigDataUrl}"\n""visibility "${visibility}"\n""alwaysZero "${alwaysZero}"\n""graphType "${graphType}"\n""windowingFunction "${windowingFunction}"\n""color "${color}"\n" >> hub.txt
+
+trackname=${name}", reverse strand"
+longLabel=${name}", reverse strand histogram tracks"
+color=100,0,0
+bigDataUrl=${name}_minus.bw
+echo -e "track "${name}_minus"\n""shortLabel "${name}"\n""longLabel "${longLabel}"\n""type "${type}"\n""bigDataUrl "${bigDataUrl}"\n""visibility "${visibility}"\\n""alwaysZero "${alwaysZero}"\n""graphType "${graphType}"\n""windowingFunction "${windowingFunction}"\n""color "${color}"\n" >> hub.txt
+
+# Other samples
+for file in "${bam_array[@]:1:47}"
+do
+    name=$(basename $file .output.bam)
+    trackname=${name}", forward strand"
+    longLabel=${name}", forward strand histogram tracks"
+    type=bigWig
+    visibility=hide
+    alwaysZero=on
+    graphType=bar
+    windowingFunction=mean
+    color=0,0,100
+    bigDataUrl=${name}_plus.bw
+    echo -e "track "${name}_plus"\n""shortLabel "${name}"\n""longLabel "${longLabel}"\n""type "${type}"\n""bigDataUrl "${bigDataUrl}"\n""visibility "${visibility}"\n""alwaysZero "${alwaysZero}"\n""graphType "${graphType}"\n""windowingFunction "${windowingFunction}"\n""color "${color}"\n" >> hub.txt
+    trackname=${name}", reverse strand"
+    longLabel=${name}", reverse strand histogram tracks"
+    color=100,0,0
+    bigDataUrl=${name}_minus.bw
+    echo -e "track "${name}_minus"\n""shortLabel "${name}"\n""longLabel "${longLabel}"\n""type "${type}"\n""bigDataUrl "${bigDataUrl}"\n""visibility "${visibility}"\n""alwaysZero "${alwaysZero}"\n""graphType "${graphType}"\n""windowingFunction "${windowingFunction}"\n""color "${color}"\n" >> hub.txt
+done
+
+# Creation the track for bigBed file
+trackname="coding_5´-end"
+longLabel="coding 5'-end track"
+type=bigBed
+visibility=full
+colorByStrand="0,0,100 100,0,0"
+bigDataUrl=coding_5end.bb
+echo -e "track "${trackname}"\n""shortLabel "${trackname}"\n""longLabel "${longLabel}"\n""type "${type}"\n""bigDataUrl "${bigDataUrl}"\n""visibility "${visibility}"\n""colorByStrand "\"${colorByStrand}\""\n" >> hub.txt
+
 # Before running this script, please make sure that you accessed allas and are able to use swift protocol. Please follow below commands.
 # module load allas
 # allas-conf <project>
@@ -104,3 +204,4 @@ echo -e "The sharable link is""\t"$link
 
 rm *.output.bam && rm *.output.bam.bai 
 rm *.bw && rm coding_5end.bb
+
